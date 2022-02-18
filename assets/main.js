502b345539d7eb2cc6bc3f7ba7d8d926610b646a
marlencar <alenc4rry@gmail.com>
2022-02-18 15:35:57 -0300

    js changes

--- assets/main.js
+++ assets/main.js
@@ -1,30 +1,40 @@
-  const form = document.getElementById('submit') 
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const resetEmail = document.getElementsByClassName("email");
-    const resetSenha = document.getElementsByClassName("password");
-    const valorEmail = resetEmail[0].value;
-    const valorSenha = resetSenha[0].value;
-    let boo = false;
-    if (!valorEmail || !valorSenha) {
-      boo = false;
-    } else boo = true;
-    console.log(boo);
-    if (boo === false) {
-      mostraErro();
-      return;
-    }
-    const cssEmail = document.querySelector(".email");
-    cssEmail.style.borderColor = "#ffffff";
-    const error = document.querySelector(".password");
-    error.innerHTML = "";
-  });
+const form = document.getElementById("submit");
+const email = document.getElementsByClassName("email");
+const senha = document.getElementsByClassName("password");
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let valorEmail = email[0].value;
+  let valorSenha = senha[0].value;
+  let boo = false;
+
+  if (!valorEmail || !valorSenha) {
+    boo = false;
+  } else boo = true;
+  console.log(boo);
+
+  if (boo === false) {
+    mostraErro();
+    return;
+  }
+
+  const cssEmail = document.querySelector(".email");
+  cssEmail.style.borderColor = "#ffffff";
+  const error = document.querySelector(".error");
+  error.innerHTML = "";
+  clearInput();
+  return;
+});
 
 function mostraErro() {
   const email = document.querySelector(".email");
+
   email.style.borderColor = "#FF5757";
   const error = document.querySelector(".error");
   error.innerHTML = "Por favor, insira um email válido";
   return;
+}
+
+function clearInput() {
+  document.getElementById("submit").reset();
 }
